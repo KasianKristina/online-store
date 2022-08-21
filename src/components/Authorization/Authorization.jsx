d6fd5ff287bc21e88bae0d8b93b1e4f32580f8c2
Kristina <kasyankn@gmail.com>
2022-08-21 18:06:26 +0300

    authorization form

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,10 +1,22 @@
 import styles from './styles.module.css'
+import classnames from 'classnames'
 
 export const Authorization = () => {
     return (
-        <>
-            <div>Авторизация</div>
-            <input placeholder='введите имя'></input>
-        </>
+        <div className={styles.auth}>
+            <div className={styles.auth_header}>Sign in</div>
+            <div className={styles.auth_form}>
+                <input className={classnames(styles.auth_form_input, styles.auth_form_element)} placeholder='Login'></input>
+                <input className={classnames(styles.auth_form_input, styles.auth_form_element)} placeholder='Password'></input>
+                <button className={classnames(styles.auth_form_enter, styles.auth_form_element)}>Sign in</button>
+                <div className={styles.auth_form_line}>
+                    <div className={styles.auth_form_line_bold}>
+                        <div className={styles.auth_form_line_text}>or</div>
+                    </div>
+                </div>
+                <button className={classnames(styles.auth_form_apple, styles.auth_form_element)}>Sign in with Apple</button>
+                <a href="#" className={styles.auth_form_mail}>Sign in with mail</a>
+            </div>
+        </div>
     )
 }
