7a4996aaff0138483bc68ae83a3b2cfc5120d19a
Kristina <kasyankn@gmail.com>
2022-08-22 16:07:06 +0300

    fix comments

--- src/components/Comments/SimpleSlider.jsx
+++ src/components/Comments/SimpleSlider.jsx
@@ -1,19 +1,36 @@
 import {Comment} from './Comment/Comment'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import React from "react";
+import React, { Component } from "react";
 import Slider from "react-slick";
 
-export default function SimpleSlider() {
-    var settings = {
+function Arrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", margin: "0px 30px"}}
+      onClick={onClick}
+    />
+  );
+}
+
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
       dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay: false,
+      nextArrow: <Arrow />,
+      prevArrow: <Arrow />,
+      className: "center",
+      centerMode: true,
+      centerPadding: "70px"
     };
     return (
-
       <Slider {...settings}>
         <div>
           <Comment />
@@ -34,6 +51,8 @@ export default function SimpleSlider() {
           <Comment />
         </div>
       </Slider>
+      
     );
   }
+}
 
